0a753835492e469613c3f48c11b4956efe6aad53
Ivana Samardzija <c.isamardzija@gogoair.com>
2017-12-26 10:08:55 +0100

    Initial commit

---
+++ src/main/java/com/rapid/prototype/excelsucks/ExcelsucksApplication.java
@@ -0,0 +1,12 @@
+package com.rapid.prototype.excelsucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelsucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelsucksApplication.class, args);
+	}
+}
