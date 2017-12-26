7f700238b839d182ceb032c05c2e37b32e524708
andrejici <ivanandrejic@yahoo.com>
2017-12-26 11:39:26 +0100

    add services

--- src/main/java/com/rapid/prototype/excelsucks/repo/OrderRepository.java
+++ src/main/java/com/rapid/prototype/excelsucks/repo/OrderRepository.java
@@ -1,8 +1,10 @@
 package com.rapid.prototype.excelsucks.repo;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rapid.prototype.excelsucks.domain.Order;
 
+@Repository
 public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
 }
