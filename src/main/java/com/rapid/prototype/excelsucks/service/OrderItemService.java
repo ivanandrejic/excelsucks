7bb550bfd08dd8183d678f289356d19c73aef9fa
Ivana Samardzija <c.isamardzija@gogoair.com>
2017-12-26 15:26:14 +0100

    Daily

--- src/main/java/com/rapid/prototype/excelsucks/service/OrderItemService.java
+++ src/main/java/com/rapid/prototype/excelsucks/service/OrderItemService.java
@@ -1,11 +1,16 @@
 package com.rapid.prototype.excelsucks.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.rapid.prototype.excelsucks.domain.Order;
 import com.rapid.prototype.excelsucks.domain.OrderItem;
 import com.rapid.prototype.excelsucks.repo.FoodItemRepository;
 import com.rapid.prototype.excelsucks.repo.OrderItemRepository;
+import com.rapid.prototype.excelsucks.web.dto.OrderDailyDTO;
 import com.rapid.prototype.excelsucks.web.dto.OrderItemDTO;
 
 import org.springframework.stereotype.Service;
@@ -29,9 +34,27 @@ public class OrderItemService {
         return orderItemRepository.save(orderItem);
     }
 
-//    public void getDailyOrders(Integer day) {
-//        List<OrderItem>
-//
-//
-//    }
+    public List<OrderDailyDTO> getDailyOrders(Integer day) {
+        List<OrderItem> orderItems = orderItemRepository.findByDay(day);
+        Map<String, OrderDailyDTO> data = new HashMap<>();
+
+        for (OrderItem orderItem : orderItems) {
+
+            orderItem.getFoodItems().forEach(fi ->
+
+                                             {
+                                                 OrderDailyDTO list = new OrderDailyDTO();
+                                                 list.setFoodName(fi.getTitle());
+                                                 if (data.containsKey(fi.getTitle())) {
+                                                     list = data.get(fi.getTitle());
+                                                 } else {
+                                                     data.put(fi.getTitle(), list);
+                                                 }
+
+                                                 list.setCount(list.getCount() + 1);
+                                                 list.getNames().add(orderItem.getName());
+                                             });
+        }
+        return new ArrayList<>(data.values());
+    }
 }
