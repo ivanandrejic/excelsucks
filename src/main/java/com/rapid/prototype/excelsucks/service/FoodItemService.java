8b5f567aaed7262a006e53de8241b7deb001d9ef
andrejici <ivanandrejic@yahoo.com>
2017-12-26 12:12:54 +0100

    add service

---
+++ src/main/java/com/rapid/prototype/excelsucks/service/FoodItemService.java
@@ -0,0 +1,11 @@
+package com.rapid.prototype.excelsucks.service;
+
+import java.util.List;
+
+import com.rapid.prototype.excelsucks.web.dto.FoodItemDTO;
+
+public interface FoodItemService {
+
+	public List<FoodItemDTO> findAll();
+	
+}
---
+++ src/main/java/com/rapid/prototype/excelsucks/service/impl/FoodItemServiceImpl.java
@@ -0,0 +1,38 @@
+package com.rapid.prototype.excelsucks.service.impl;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rapid.prototype.excelsucks.domain.FoodItem;
+import com.rapid.prototype.excelsucks.repo.FoodItemRepository;
+import com.rapid.prototype.excelsucks.service.FoodItemService;
+import com.rapid.prototype.excelsucks.web.dto.FoodItemDTO;
+
+@Service
+public class FoodItemServiceImpl implements FoodItemService {
+
+	@Autowired
+	FoodItemRepository foodRepo;
+	
+	@Override
+	public List<FoodItemDTO> findAll() {
+		
+		List<FoodItemDTO> result = new ArrayList<FoodItemDTO>();
+		for(FoodItem item: foodRepo.findAll()) {
+			FoodItemDTO dto = new FoodItemDTO();
+			dto.setDescription(item.getDescription());
+			dto.setId(item.getId());
+			dto.setPhotoURI(item.getPhotoUri());
+			dto.setPrice(new BigDecimal(item.getPrice()));
+			dto.setTitle(item.getTitle());
+		}
+		return result;
+	}
+	
+	
+
+}
