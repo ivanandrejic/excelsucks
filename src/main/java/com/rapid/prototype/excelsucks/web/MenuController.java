147d9761420bbaa2282d53814965bd29203343ed
Avramovic Slavisa <avramovic.slavisa@partake.de>
2017-12-26 13:35:02 +0100

    Chaing the response of MenuController

--- src/main/java/com/rapid/prototype/excelsucks/web/MenuController.java
+++ src/main/java/com/rapid/prototype/excelsucks/web/MenuController.java
@@ -1,9 +1,7 @@
 package com.rapid.prototype.excelsucks.web;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import com.rapid.prototype.excelsucks.service.FoodItemService;
+import com.rapid.prototype.excelsucks.web.dto.FoodItemDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rapid.prototype.excelsucks.service.FoodItemService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 //import com.rapid.prototype.excelsucks.web.dto.FoodItemBuilder;
-import com.rapid.prototype.excelsucks.web.dto.FoodItemDTO;
-import com.rapid.prototype.excelsucks.web.dto.SummaryFoodList;
 
 /**
  * @author <a href="mailto:slavisa.avramovic@escriba.de">avramovics</a>
@@ -29,18 +28,18 @@ public class MenuController {
 	FoodItemService foodService;
 
     @RequestMapping(value = {"", "/"}, method = RequestMethod.GET )
-    public Map<Integer, List<FoodItemDTO>> getFoodList() {
-        Map<Integer, List<FoodItemDTO>> foodItemDTOS = foodService.findAllMap();
-        return foodItemDTOS;
+    public Map<Long, List<FoodItemDTO>> getFoodList() {
+        List<FoodItemDTO> foodItemDTOS = foodService.findAll();
+        Map<Long, List<FoodItemDTO>>  mFoodList = new HashMap<>();
+        mFoodList.put(1l, foodItemDTOS);
+        return mFoodList;
     }
 
     @RequestMapping(value = {"/day/{day}"}, method = RequestMethod.GET )
-    public Map<Integer, SummaryFoodList> getFoodList(@PathVariable Integer day) {
+    public Map<Long, List<FoodItemDTO>> getFoodList(@PathVariable Integer day) {
         List<FoodItemDTO> foodItemDTOS = foodService.findByDay(day);
-        SummaryFoodList summaryFoodList = new SummaryFoodList();
-        summaryFoodList.setFoodItems(foodItemDTOS);
-        Map<Integer, SummaryFoodList>  mFoodList = new HashMap<>();
-        mFoodList.put(day, summaryFoodList);
+        Map<Long, List<FoodItemDTO>>  mFoodList = new HashMap<>();
+        mFoodList.put(1l, foodItemDTOS);
         return mFoodList;
     }
 
